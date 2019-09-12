4e2f4bb9ba41b2035b6ee1316d08a55be9da08e2
futuretea <1913508671@qq.com>
2019-09-12 16:12:20 +0800

    add dockernet.sh

---
+++ namespace/dockernet.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+useage(){
+    echo "useage:"
+    echo "  dockernet.sh CONTAINER COMMAND [ARG]..."
+}
+
+if [ $# -lt 2 ];then
+    useage
+    exit
+fi
+
+CONTAINER=$1
+shift 1
+PID=$(docker inspect --format "{{.State.Pid}}" "${CONTAINER}")
+mkdir -p /var/run/netns
+rm /var/run/netns/"${CONTAINER}"
+ln -s /proc/"${PID}"/ns/net /var/run/netns/"${CONTAINER}"
+ip netns exec ${CONTAINER} $@

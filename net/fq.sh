4ffb559aa79d53406f427f60eadf9bbca2ef9939
futuretea <1913508671@qq.com>
2019-09-19 00:54:00 +0800

    add fq.sh

---
+++ net/fq.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+set -e
+
+useage(){
+    echo "useage:"
+    echo "  fq.sh APP ACTION [URL]"
+}
+
+if [ $# -lt 2 ];then
+    useage
+    exit
+fi
+
+APP=$1
+ACTION=$2
+URL=${3:-"http://127.0.0.1:12333"}
+
+shell_set(){
+echo export http_proxy="${URL}"
+echo export https_proxy="${URL}"
+}
+
+shell_unset(){
+echo export http_proxy=""
+echo export https_proxy=""
+}
+
+git_set(){
+git config --global http.proxy "${URL}"
+git config --global https.proxy "${URL}"
+}
+
+git_unset(){
+git config --global http.proxy --unset
+git config --global https.proxy --unset
+}
+
+if [ "x${APP}" == "xgit" ];then
+    if [ "x${ACTION}" == "x+"  ];then
+        git_set
+    fi
+    if [ "x${ACTION}" == "x-"  ];then
+        git_unset
+    fi
+fi
+if [ "x${APP}" == "xshell" ];then
+    if [ "x${ACTION}" == "x+"  ];then
+        shell_set
+    fi
+    if [ "x${ACTION}" == "x-"  ];then
+        shell_unset
+    fi
+fi

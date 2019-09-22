83c66085eb32c47325d70bdcce95c334434dee85
futuretea <1913508671@qq.com>
2019-09-22 16:22:57 +0800

    update k8s/kubefetch.sh

--- k8s/kubefetch.sh
+++ k8s/kubefetch.sh
@@ -3,14 +3,27 @@ set -e
 
 useage(){
     echo "useage:"
-    echo "  kubefetch.sh NAME [HOST]"
+    echo "  kubefetch.sh SSH_CONFIG CLUSTER [HOST] [PORT]"
+    echo "eg:"
+    echo "  kubefetch dev{,}"
+    echo "  kubefetch dev{,,}"
+    echo "  kubefetch dev{,,} 8443"
+    echo "  kubefetch tom-pc dev dev-master1 18888"
 }
 
-if [ $# -lt 1 ];then
+if [ $# -lt 2 ];then
     useage
     exit
 fi
+SSH_CONFIG=$1
+CLUSTER=$2
+HOST=$3
+PORT=$4
 
-NAME=$1
-HOST=${2:-$1}
-ssh root@"${HOST}" cat /root/.kube/config | sed "s/server: https:\/\/.*:6443/server: https:\/\/${HOST}:6443/g" >~/.kube/config."${NAME}"
+if [ $# -eq 2 ];then
+    ssh "${SSH_CONFIG}" cat /root/.kube/config >~/.kube/config."${CLUSTER}"
+elif [ $# -eq 3 ];then
+    ssh "${SSH_CONFIG}" cat /root/.kube/config | sed "s/server: https:\/\/.*:/server: https:\/\/${HOST}:/g"  >~/.kube/config."${CLUSTER}"
+elif [ $# -eq 4 ];then
+    ssh "${SSH_CONFIG}" cat /root/.kube/config | sed "s/server: https:\/\/.*/server: https:\/\/${HOST}:${PORT}/g" >~/.kube/config."${CLUSTER}"
+fi

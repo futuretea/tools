477c2c3c8987019dccc80636f2dc5ccf8892f2c8
futuretea <1913508671@qq.com>
2019-09-25 17:14:16 +0800

    add k8s/kubecheck.sh

---
+++ k8s/kubecheck.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+[[ -n $DEBUG ]] && set -x
+set -eou pipefail
+
+useage(){
+  cat <<"EOF"
+USAGE:
+    kubecheck.sh 
+EOF
+}
+
+exit_err() {
+   echo >&2 "${1}"
+   exit 1
+}
+
+if [ $# -lt 0 ];then
+    useage
+    exit 1
+fi
+
+check_node(){
+  kubectl get node --field-selector=status.phase!=Running
+}
+check_cs(){
+  kubectl get cs
+}
+
+check_node
+check_cs

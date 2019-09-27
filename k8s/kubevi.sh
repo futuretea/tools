97d630864fa6ad3076e0db4550298bab7ec99c6d
futuretea <1913508671@qq.com>
2019-09-27 15:53:10 +0800

    add prompt

--- k8s/kubevi.sh
+++ k8s/kubevi.sh
@@ -18,7 +18,10 @@ REMOTE=$4
 LOCAL=$(basename "${REMOTE}")
 
 kubectl -n "${NAMESPACE}" -controller cp "${POD}":"${REMOTE}" "${LOCAL}" -c "${CONTAINER}"
+LOCALMD51=$(md5sum "${LOCAL}" | awk '{print $1}')
 vi "${LOCAL}"
+LOCALMD52=$(md5sum "${LOCAL}" | awk '{print $1}')
+if [ x"${LOCALMD51}" != x"${LOCALMD52}" ];then
 kubectl -n "${NAMESPACE}" -controller cp "${LOCAL}" "${POD}":"${REMOTE}" -c "${CONTAINER}"
+fi
 rm "${LOCAL}"
-

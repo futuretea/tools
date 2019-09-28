c2208de2b887e41242e3806b90ff9f34dc998933
futuretea <1913508671@qq.com>
2019-09-29 05:08:23 +0800

    move goenvinit(alias) to goe(script)

---
+++ go/goe.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+[[ -n $DEBUG ]] && set -x
+set -eou pipefail
+
+useage(){
+  cat <<"EOF"
+USAGE:
+    goe.sh GOMODULE PMMODE(mod,other) PROXYMODE(mod,other)
+EOF
+}
+
+exit_err() {
+   echo >&2 "${1}"
+   exit 1
+}
+
+if [ $# -lt 3 ];then
+    useage
+    exit 1
+fi
+
+GOMODULE=$1
+PMMODE=${2-"mod"}
+PROXYMODE=${3-"mod"}
+if [ x"${PMMODE}" == "xmod" ];then
+    USEGOMOD=true
+else
+    USEGOMOD=false
+fi
+if [ x"${PROXYMODE}" == "xmod" ];then
+    USEPROXY=gomod
+    NOPROXY=git,sh
+else
+    USEPROXY=git,sh
+    NOPROXY=gomod
+fi
+GOVERSIONNOW="$(go version | awk '{print $3}' | sed 's/go//g')"
+cat >go.env<<EOF
+GOMODULE=${GOMODULE}
+GOVERSION=${GOVERSIONNOW}
+USEGOMOD=${USEGOMOD}
+USEPROXY=${USEPROXY}
+NOPROXY=${NOPROXY}
+EOF

801d80b70f099f6edccf271eb98b3e6cc3cc5a74
futuretea <1913508671@qq.com>
2019-09-30 10:03:15 +0800

    add rust

---
+++ rust/install_rust.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+[[ -n $DEBUG ]] && set -x
+set -eou pipefail
+
+useage(){
+  cat <<"EOF"
+USAGE:
+    install_rust.sh 
+EOF
+}
+
+exit_err() {
+   echo >&2 "${1}"
+   exit 1
+}
+
+if [ $# -lt 0 ];then
+    useage
+    exit 1
+fi
+
+proxychains curl https://sh.rustup.rs -sSf | sh
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -24,6 +24,7 @@ export EDITOR='vim'
 setopt no_nomatch
 source $ZSH/oh-my-zsh.sh
 source /usr/local/bin/alias/all
+source $HOME/.cargo/env
 eval "$(kubectl completion zsh)"
 eval "$(doctl completion zsh)"
 eval "$(starship init zsh)"

191f85891ac7f36a2416bb171aed02bdd022df01
futuretea <1913508671@qq.com>
2019-10-02 22:13:14 +0800

    add cssh.sh

---
+++ other/cssh.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+[[ -n $DEBUG ]] && set -x
+set -eou pipefail
+
+useage(){
+  cat <<"EOF"
+USAGE:
+    cssh.sh
+EOF
+}
+
+exit_err() {
+   echo >&2 "${1}"
+   exit 1
+}
+
+if [ $# -lt 0 ];then
+    useage
+    exit 1
+fi
+
+docker exec -it ssr cat /etc/shadowsocksr.json | jq -r '.server, .server_port' |xargs alive
+

8b6d974fb2ec316bedc1195c72ec4dc89e8fec8b
futuretea <1913508671@qq.com>
2019-10-13 22:50:58 +0800

    gov support download go

--- go/gov.sh
+++ go/gov.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 [[ -n $DEBUG ]] && set -x
-set -ou pipefail
+set -eou pipefail
 
 useage(){
   cat <<"EOF"
@@ -15,7 +15,19 @@ if [ $# -lt 1 ];then
 fi
 
 VERSION=$1
+if [ -z "${GOROOT}" ];then
+  GOROOT="/usr/local/bin/go"
+fi
 sudo rm -f "${GOROOT}"
-sudo ln -s "${GOROOT}${VERSION}" "${GOROOT}"
+GOPACK="${GOROOT}${VERSION}"
+if [ ! -d "${GOPACK}" ];then
+  sudo wget "https://dl.google.com/go/go${VERSION}.linux-amd64.tar.gz" -cP "${HOME}/Downloads/"
+  TEMPDIR=$(mktemp -d)
+  tar -zxf "${HOME}/Downloads/go${VERSION}.linux-amd64.tar.gz" -C "${TEMPDIR}"
+  mkdir -p "${TEMPDIR}"
+  sudo mv "${TEMPDIR}/go" "${GOPACK}"
+  sudo rm -r "${TEMPDIR}"
+fi
+sudo ln -s "${GOPACK}" "${GOROOT}"
 GOVERSIONNOW="$(go version | awk '{print $3}' | sed 's/go//g')"
 echo "go ${GOVERSIONNOW}"

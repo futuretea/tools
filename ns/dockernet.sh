a2dde9663540ae56f61aeb01c2171ed50183264d
futuretea <1913508671@qq.com>
2019-11-14 17:54:12 +0800

    fix ns/dockernet.sh

--- ns/dockernet.sh
+++ ns/dockernet.sh
@@ -15,6 +15,6 @@ CONTAINER=$1
 shift 1
 PID=$(docker inspect --format "{{.State.Pid}}" "${CONTAINER}")
 mkdir -p /var/run/netns
-rm /var/run/netns/"${CONTAINER}"
+rm -rf /var/run/netns/"${CONTAINER}"
 ln -s /proc/"${PID}"/ns/net /var/run/netns/"${CONTAINER}"
 ip netns exec "${CONTAINER}" $@

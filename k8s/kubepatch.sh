1175b9d6013a20794903aebd303a40961671ad8b
futuretea <1913508671@qq.com>
2019-11-19 15:58:45 +0800

    add kubepatch.sh

---
+++ k8s/kubepatch.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+[[ -n $DEBUG ]] && set -x
+set -eou pipefail
+
+useage(){
+  cat <<HELP
+USAGE:
+    new TYPE WORKLOAD CONTAINER IMAGE
+HELP
+}
+
+exit_err() {
+   echo >&2 "${1}"
+   exit 1
+}
+
+if [ $# -lt 4 ];then
+    useage
+    exit 1
+fi
+
+TYPE=$1
+WORKLOAD=$2
+CONTAINER=$3
+IMAGE=$4
+
+SPEC='{"spec": {"template": {"spec": {"containers": [{"name": "'"${CONTAINER}"'","image": "'"${IMAGE}"'"}]}}}}'
+
+kubectl patch "${TYPE}" "${WORKLOAD}" --patch "${SPEC}"
+  

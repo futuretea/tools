8d15eba2d026230924ac4d883789d569be4b083b
futuretea <1913508671@qq.com>
2019-12-05 15:10:12 +0800

    add kubeprobe

---
+++ k8s/kubeprobe.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+[[ -n $DEBUG ]] && set -x
+set -eou pipefail
+
+useage(){
+  cat <<HELP
+USAGE:
+    new TYPE WORKLOAD CONTAINER path port initialDelaySeconds [periodSeconds timeoutSeconds successThreshold failureThreshold]
+    eg: kubeprobe deploy ui nginx / 80 3 2 1 1 3
+HELP
+}
+
+exit_err() {
+   echo >&2 "${1}"
+   exit 1
+}
+
+if [ $# -lt 10 ];then
+    useage
+    exit 1
+fi
+
+TYPE=$1
+WORKLOAD=$2
+CONTAINER=$3
+path=$4
+port=$5
+initialDelaySeconds=$6
+periodSeconds=${7:-10}
+timeoutSeconds=${8:-1}
+successThreshold=${9:-1}
+failureThreshold=${10:-3}
+
+PROBE='{"failureThreshold": '$failureThreshold',"httpGet": {"'$path'": "/","port": '$port',"scheme": "HTTP"},"initialDelaySeconds": '$initialDelaySeconds',"periodSeconds": '$periodSeconds',"successThreshold": '$successThreshold',"timeoutSeconds": '$timeoutSeconds'}'
+
+SPEC='{"spec": {"template": {"spec": {"containers": [{"name": "'${CONTAINER}'","livenessProbe":'${PROBE}',"readinessProbe":'${PROBE}'}]}}}}'
+
+kubectl patch "${TYPE}" "${WORKLOAD}" --patch "${SPEC}"

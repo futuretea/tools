e1f7293ddbc71af169005f8d900a8ebdba968e11
futuretea <1913508671@qq.com>
2019-12-09 11:38:40 +0800

    add kubeprobe

--- k8s/kubeprobe.sh
+++ k8s/kubeprobe.sh
@@ -5,8 +5,11 @@ set -eou pipefail
 useage(){
   cat <<HELP
 USAGE:
-    kubeprobe.sh TYPE WORKLOAD CONTAINER probe path port initialDelaySeconds [periodSeconds timeoutSeconds successThreshold failureThreshold]
-    eg: kubeprobe deploy ui nginx / 80 3 2 1 1 3
+    kubeprobe.sh TYPE WORKLOAD CONTAINER [LIVENESSPROBE READINESSPROBE]
+    eg: 
+      kubeprobe deploy ui nginx
+      kubeprobe deploy ui nginx "" "/ 80 3 2 1 1 3"
+      kubeprobe deploy ui nginx "/ 80 10 2 1 1 3" "/ 80 3 2 1 1 3"
 HELP
 }
 
@@ -15,7 +18,7 @@ exit_err() {
    exit 1
 }
 
-if [ $# -lt 10 ];then
+if [ $# -lt 3 ];then
     useage
     exit 1
 fi
@@ -23,16 +26,25 @@ fi
 TYPE=$1
 WORKLOAD=$2
 CONTAINER=$3
-path=$4
-port=$5
-initialDelaySeconds=$6
-periodSeconds=${7:-10}
-timeoutSeconds=${8:-1}
-successThreshold=${9:-1}
-failureThreshold=${10:-3}
+LIVENESSPROBE_CONFIG=${4:-""}
+READINESSPROBE_CONFIG=${5:-""}
 
-PROBE='{"failureThreshold": '$failureThreshold',"httpGet": {"'$path'": "/","port": '$port',"scheme": "HTTP"},"initialDelaySeconds": '$initialDelaySeconds',"periodSeconds": '$periodSeconds',"successThreshold": '$successThreshold',"timeoutSeconds": '$timeoutSeconds'}'
+SPEC='{"spec": {"template": {"spec": {"containers": [{"name": "'${CONTAINER}'"'
 
-SPEC='{"spec": {"template": {"spec": {"containers": [{"name": "'${CONTAINER}'","livenessProbe":'${PROBE}',"readinessProbe":'${PROBE}'}]}}}}'
-
+if [ -n "${LIVENESSPROBE_CONFIG}" ];then
+  LIVENESSPROBE=(${LIVENESSPROBE_CONFIG})
+  LIVENESSPROBE_SPEC='{"httpGet": {"path": "'${LIVENESSPROBE[0]}'","port": '${LIVENESSPROBE[1]}',"scheme": "HTTP"},"initialDelaySeconds": '${LIVENESSPROBE[2]}',"periodSeconds": '${LIVENESSPROBE[3]}',"successThreshold": '${LIVENESSPROBE[4]}',"timeoutSeconds": '${LIVENESSPROBE[5]}',"failureThreshold": '${LIVENESSPROBE[6]}'}'
+  SPEC=${SPEC}',"livenessProbe":'${LIVENESSPROBE_SPEC}
+else
+  SPEC=${SPEC}',"livenessProbe":null'
+fi
+if [ -n "${READINESSPROBE_CONFIG}" ];then
+  READINESSPROBE=(${READINESSPROBE_CONFIG})
+  READINESSPROBE_SPEC='{"httpGet": {"path": "'${READINESSPROBE[0]}'","port": '${READINESSPROBE[1]}',"scheme": "HTTP"},"initialDelaySeconds": '${READINESSPROBE[2]}',"periodSeconds": '${READINESSPROBE[3]}',"successThreshold": '${READINESSPROBE[4]}',"timeoutSeconds": '${READINESSPROBE[5]}',"failureThreshold": '${READINESSPROBE[6]}'}'
+  SPEC=${SPEC}',"readinessProbe":'${READINESSPROBE_SPEC}
+else
+  SPEC=${SPEC}',"readinessProbe":null'
+fi
+SPEC=${SPEC}'}]}}}}'
+echo "${SPEC}"
 kubectl patch "${TYPE}" "${WORKLOAD}" --patch "${SPEC}"

f630e152640d0dc7a88300c03e71af18f321347d
futuretea <1913508671@qq.com>
2019-12-13 22:43:45 +0800

    add webdemo.sh

---
+++ web/webdemo.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+[[ -n $DEBUG ]] && set -x
+set -eou pipefail
+
+useage(){
+  cat <<HELP
+USAGE:
+    webdemo.sh
+HELP
+}
+
+exit_err() {
+   echo >&2 "${1}"
+   exit 1
+}
+
+if [ $# -lt 0 ];then
+    useage
+    exit 1
+fi
+
+DEMODIR=$(mktemp -d)
+echo "${DEMODIR}"
+cd "${DEMODIR}"
+cat > index.html <<EOF
+<html>
+
+<body>
+
+    <h1>My First Heading</h1>
+
+    <p>My first paragraph.</p>
+
+</body>
+
+</html>
+EOF
+xdg-open index.html >/dev/null 2>&1
+code .

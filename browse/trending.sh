5d6c6aaebfd3f07d842421db29c92e1b77ba0327
futuretea <1913508671@qq.com>
2019-12-14 22:15:37 +0800

    add trending

---
+++ browse/trending.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+[[ -n $DEBUG ]] && set -x
+set -eou pipefail
+
+useage(){
+  cat <<HELP
+USAGE:
+    trending.sh LANG
+HELP
+}
+
+exit_err() {
+   echo >&2 "${1}"
+   exit 1
+}
+
+if [ $# -lt 1 ];then
+    useage
+    exit 1
+fi
+
+LANG=$1
+open https://github.com/trending/"${LANG}"?since=daily

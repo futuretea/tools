2b042d0c3907ebe3a2ff59e4de572a53494c768a
futuretea <1913508671@qq.com>
2019-12-14 22:11:19 +0800

    update install_vagrant_libvirt.sh

--- vm/install_vagrant_libvirt.sh
+++ vm/install_vagrant_libvirt.sh
@@ -19,5 +19,7 @@ if [ $# -lt 0 ];then
     exit 1
 fi
 
+if [ ! -L /usr/include/libxml ];then
 ln -s /usr/include/libxml2/libxml /usr/include/libxml
+fi
 CONFIGURE_ARGS='with-ldflags=-L/opt/vagrant/embedded/lib with-libvirt-include=/usr/include/libvirt with-libvirt-lib=/usr/lib' GEM_HOME=~/.vagrant.d/gems GEM_PATH=$GEM_HOME:/opt/vagrant/embedded/gems PATH=/opt/vagrant/embedded/bin:$PATH proxychains vagrant plugin install vagrant-libvirt

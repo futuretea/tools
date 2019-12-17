4ab04901b278f9ccee8ceb58c11ea2e676c507ec
futuretea <1913508671@qq.com>
2019-12-17 18:49:13 +0800

    add vmimage.sh

---
+++ vm/vmimage.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+[[ -n $DEBUG ]] && set -x
+set -eou pipefail
+
+useage(){
+  cat <<HELP
+USAGE:
+    vmimage.sh HOST NAME VERSION
+HELP
+}
+
+exit_err() {
+   echo >&2 "${1}"
+   exit 1
+}
+
+if [ $# -lt 2 ];then
+    useage
+    exit 1
+fi
+
+HOST=$1
+NAME=$2
+VERSION=${3:-"latest"}
+TEMPDIR=$(mktemp -d)
+cd "${TEMPDIR}"
+ssh "${HOST}" tar -cvpf /tmp/system.tar --directory=/ --exclude=proc --exclude=sys --exclude=dev --exclude=run --exclude=boot .
+scp "${HOST}":/tmp/system.tar .
+docker import system.tar "${NAME}:${VERSION}"
+docker save "${NAME}:${VERSION}" -o "./${NAME}-${VERSION}.tar"

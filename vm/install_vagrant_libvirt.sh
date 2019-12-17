479a487dba504c5347f49139c96f28dd13e6d0bc
futuretea <1913508671@qq.com>
2019-12-18 01:09:44 +0800

    shfmt

--- vm/install_vagrant_libvirt.sh
+++ vm/install_vagrant_libvirt.sh
@@ -2,24 +2,24 @@
 [[ -n $DEBUG ]] && set -x
 set -eou pipefail
 
-useage(){
-  cat <<"EOF"
+useage() {
+    cat <<"EOF"
 USAGE:
     install_vagrant_libvirt.sh
 EOF
 }
 
 exit_err() {
-   echo >&2 "${1}"
-   exit 1
+    echo >&2 "${1}"
+    exit 1
 }
 
-if [ $# -lt 0 ];then
+if [ $# -lt 0 ]; then
     useage
     exit 1
 fi
 
-if [ ! -L /usr/include/libxml ];then
-ln -s /usr/include/libxml2/libxml /usr/include/libxml
+if [ ! -L /usr/include/libxml ]; then
+    ln -s /usr/include/libxml2/libxml /usr/include/libxml
 fi
 CONFIGURE_ARGS='with-ldflags=-L/opt/vagrant/embedded/lib with-libvirt-include=/usr/include/libvirt with-libvirt-lib=/usr/lib' GEM_HOME=~/.vagrant.d/gems GEM_PATH=$GEM_HOME:/opt/vagrant/embedded/gems PATH=/opt/vagrant/embedded/bin:$PATH proxychains vagrant plugin install vagrant-libvirt

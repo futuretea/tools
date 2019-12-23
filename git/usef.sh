1aad2370983f3697a9fcca0892aac6ccece1ef43
futuretea <1913508671@qq.com>
2019-12-23 15:39:22 +0800

    add usef.sh

---
+++ git/usef.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+[[ -n $DEBUG ]] && set -x
+set -eou pipefail
+
+useage() {
+    cat <<HELP
+USAGE:
+    usef.sh FILE 
+HELP
+}
+
+exit_err() {
+    echo >&2 "${1}"
+    exit 1
+}
+
+if [ $# -lt 0 ]; then
+    useage
+    exit 1
+fi
+FILE=${1:-$(fzf)}
+BRANCH=$(git branch -l -vv | fzf | awk '{print $1}')
+if [ x"${BRANCH}" != "x*" ];then
+    if [ -n "${FILE}" ];then
+    echo "git checkout ${BRANCH} -- ${FILE}"
+    fi
+fi

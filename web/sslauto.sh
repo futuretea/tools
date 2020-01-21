036cc087e4d7dba5db957383a7e3b3a90c3a524c
futuretea <1913508671@qq.com>
2020-01-21 14:03:21 +0800

    add sslauto.sh

---
+++ web/sslauto.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+[[ -n $DEBUG ]] && set -x
+set -eou pipefail
+
+useage() {
+    cat <<HELP
+USAGE:
+    sslauto.sh WEBROOT EMAIL DOMAIN [SUBS...]
+HELP
+}
+
+exit_err() {
+    echo >&2 "${1}"
+    exit 1
+}
+
+if [ $# -lt 3 ]; then
+    useage
+    exit 1
+fi
+
+WEBROOT=$1
+EMAIL=$2
+DOMAIN=$3
+shift 3
+SUBS=$@
+sudo openresty -s reload
+ARGS="--webroot -w ${WEBROOT} --email ${EMAIL} -d ${DOMAIN}"
+for SUB in $SUBS;do
+	ARGS="$ARGS -d ${SUB}.${DOMAIN}"
+done
+sudo certbot certonly ${ARGS}

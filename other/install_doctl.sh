b7ab08447e498e9cb237aac89bf1539c2e0c1949
futuretea <1913508671@qq.com>
2020-02-11 11:58:20 +0800

    update install_doctl

--- other/install_doctl.sh
+++ other/install_doctl.sh
@@ -5,7 +5,7 @@ set -eou pipefail
 useage() {
     cat <<HELP
 USAGE:
-    install_doctl.sh VERSION
+    install_doctl.sh [VERSION]
 HELP
 }
 
@@ -14,10 +14,16 @@ exit_err() {
     exit 1
 }
 
-if [ $# -lt 1 ]; then
+if [ $# -lt 0 ]; then
     useage
     exit 1
 fi
 
-VERSION=$1
-docker run -it --rm -v /usr/local/bin:/mnt --entrypoint="" digitalocean/doctl:"${VERSION}" cp /app/doctl /mnt
+VERSION=${1:-"1.38.0"}
+# docker run -it --rm -v /usr/local/bin:/mnt --entrypoint="" digitalocean/doctl:"${VERSION}" cp /app/doctl /mnt
+TEMPDIR="$(mktemp -d)"
+cd "${TEMPDIR}"
+wget https://github.com/digitalocean/doctl/releases/download/v$VERSION/doctl-$VERSION-linux-amd64.tar.gz
+tar -zxvf doctl-$VERSION-linux-amd64.tar.gz
+cp doctl /usr/local/bin/
+

e9f268f2de104a9d743758b59ab737d1e2f14677
futuretea <1913508671@qq.com>
2020-02-12 23:16:00 +0800

    add addremote

---
+++ git/addremote.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+[[ -n $DEBUG ]] && set -x
+set -eou pipefail
+
+useage() {
+    cat <<HELP
+USAGE:
+    addremote.sh NAME HOST
+HELP
+}
+
+exit_err() {
+    echo >&2 "${1}"
+    exit 1
+}
+
+if [ $# -lt 2 ]; then
+    useage
+    exit 1
+fi
+
+NAME=$1
+HOST=$2
+REMOTE=$(git remote get-url --all origin | sed "s/git@.*:/git@git.$HOST:/")
+git remote add $NAME $REMOTE

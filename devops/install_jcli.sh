3ab0f94785a6d53dcb81d465782a1beec46b9f8f
futuretea <1913508671@qq.com>
2020-02-14 21:36:24 +0800

    add install_jcli.sh

---
+++ devops/install_jcli.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+[[ -n $DEBUG ]] && set -x
+set -eou pipefail
+
+useage() {
+    cat <<HELP
+USAGE:
+    install_jcli.sh
+HELP
+}
+
+exit_err() {
+    echo >&2 "${1}"
+    exit 1
+}
+
+if [ $# -lt 0 ]; then
+    useage
+    exit 1
+fi
+TEMPDIR="$(mktemp -d)"
+cd "${TEMPDIR}"
+wget https://github.com/jenkins-zh/jenkins-cli/releases/latest/download/jcli-linux-amd64.tar.gz
+tar -zxvf jcli-linux-amd64.tar.gz
+sudo mv jcli /usr/local/bin/
+cd -
+rm -r "${TEMPDIR}"

bd9f325427ec9d5428f688d95a547b7c4f9cbfee
futuretea <1913508671@qq.com>
2020-02-14 14:03:55 +0800

    add kubepfx.sh

---
+++ k8s/kubepfx.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+[[ -n $DEBUG ]] && set -x
+set -eou pipefail
+
+useage() {
+    cat <<HELP
+USAGE:
+    kubepfx.sh
+HELP
+}
+
+exit_err() {
+    echo >&2 "${1}"
+    exit 1
+}
+
+if [ $# -lt 0 ]; then
+    useage
+    exit 1
+fi
+
+kubekey(){
+    local FIELD=$1
+    local FILE=$2
+    cat ~/.kube/config  | grep "$FIELD" | awk -F ": " '{print $2}' | base64 -d > $FILE
+}
+
+mkdir -p ./crt
+kubekey certificate-authority-data crt/ca.crt
+kubekey client-authority-data crt/client.crt
+kubekey client-key-data crt/client.key
+openssl pkcs12 -export -out ./crt/cert.pfx -inkey ./crt/client.key -in ./crt/client.crt -certfile ./crt/ca.crt

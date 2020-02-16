8f51ad75f2c58422282cc2bccaa06ce139aa210b
futuretea <1913508671@qq.com>
2020-02-17 00:13:32 +0800

    add beegorouter.sh

---
+++ go/beegorouter.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+[[ -n $DEBUG ]] && set -x
+set -eou pipefail
+
+useage() {
+    cat <<HELP
+USAGE:
+    beegorouter.sh IN OUT
+HELP
+}
+
+exit_err() {
+    echo >&2 "${1}"
+    exit 1
+}
+
+if [ $# -gt 2 ]; then
+    useage
+    exit 1
+fi
+
+IN=${1:-"router.go"}
+OUT=${2:-"router.dot"}
+cat > "$OUT" <<EOF
+digraph router {
+    rankdir=LR
+    node [shape="record"];
+    edge [style="dashed"];
+EOF
+grep beego.Router "$IN" | grep -o "(.*)" | sed "s/[\(\)\",]/ /g" |  awk '{gsub(/:/," ",$3);print $0}' | awk '{printf "\t\"%s[%s]\" -> \"%s::%s\" -> \"%s\";\n",$1,$3,$2,$4,$2}' >> "$OUT"
+cat >> "$OUT" <<EOF
+}
+EOF

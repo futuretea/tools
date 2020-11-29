f4de5bae2a79cc4dda53a37b5b7614b4cada7d38
futuretea <1913508671@qq.com>
2020-11-29 21:09:56 +0800

    move kubefetch to kf and add sai

---
+++ k8s/kf.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+[[ -n $DEBUG ]] && set -x
+set -xou pipefail
+
+useage() {
+    cat <<HELP
+USAGE:
+    kf.sh SSH_CONFIG CLUSTER
+HELP
+}
+
+exit_err() {
+    echo >&2 "${1}"
+    exit 1
+}
+
+if [ $# -lt 2 ]; then
+    useage
+    exit 1
+fi
+
+SSH_CONFIG=$1
+CLUSTER=$2
+HOST=${3:-""}
+PORT=${4:-""}
+
+KUBECONFIGS=(
+    "/etc/rancher/k3s/k3s.yaml"
+    "/root/.kube/config"
+)
+KUBECONFIGFILE=""
+for ((i = 1; i <= ${#KUBECONFIGS[@]}; i++)); do
+    ssh "${SSH_CONFIG}" "[[ -f ${KUBECONFIGS[$i - 1]} ]]"
+    if [ $? -eq 0 ]; then
+        KUBECONFIGFILE=${KUBECONFIGS[$i - 1]}
+        break
+    fi
+done
+LOCALKUBECONFIGFILE="${HOME}/.kube/config.${CLUSTER}"
+ssh "${SSH_CONFIG}" cat ${KUBECONFIGFILE} >"${LOCALKUBECONFIGFILE}"
+if [ $# -eq 3 ]; then
+    sed -i "s/server: https:\/\/.*:/server: https:\/\/${HOST}:/g" "${LOCALKUBECONFIGFILE}"
+elif [ $# -eq 4 ]; then
+    sed -i "s/server: https:\/\/.*/server: https:\/\/${HOST}:${PORT}/g" "${LOCALKUBECONFIGFILE}"
+fi

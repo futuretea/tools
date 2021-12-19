aed486880d80a96370ca470a00662a32bd32f4fb
futuretea <1913508671@qq.com>
2021-12-19 23:03:16 +0800

    change Makefile to install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo ./shell/load.sh .
+sudo ./alias/src.sh .
+cp zsh/.zshrc ~
+cp zsh/.bash_aliases ~
+cp zsh/.tmux.conf ~
+mkdir -p ~/.config
+cp zsh/starship.toml ~/.config/
+./zsh/install_zsh_plugin.sh

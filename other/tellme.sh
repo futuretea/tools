fd3544c025072c0c3a0952268b5b248fa4b6d96f
futuretea <1913508671@qq.com>
2022-04-12 03:38:26 +0800

    add tellme

---
+++ other/tellme.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+[[ -n $DEBUG ]] && set -x
+set -eou pipefail
+
+usage() {
+    cat <<HELP
+USAGE:
+    tellme.sh sendkey title content
+HELP
+}
+
+exit_err() {
+    echo >&2 "${1}"
+    exit 1
+}
+
+if [ $# -lt 3 ]; then
+    usage
+    exit 1
+fi
+
+sendkey=$1
+title=$2
+content=$3
+
+curl -fk https://sctapi.ftqq.com/${sendkey}.send?title=${title}&desp=${content}

d7982e0eb60cbdfb04169c5c0a535b8065c9eca6
futuretea <1913508671@qq.com>
2022-11-21 15:05:51 +0800

    update dell_t620_fan_speed_controler

--- devops/dell_t620_fan_speed_controler.sh
+++ devops/dell_t620_fan_speed_controler.sh
@@ -27,9 +27,9 @@ snmp_host=$1
 snmp_ver="2c"
 snmp_com="public"
 
-# 三个温度值将转速划分为四个区间
-temp_threshold="40,50,58,60,65"
-fan_speed="10,20,30,40,70,100"
+# 三个温度值将转速划分区间,请结合自己的风扇情况调整
+temp_threshold="40,50,60,65"
+fan_speed="10,15,20,50"
 
 # 日志文件路径
 log_dir="${HOME}/.cache"
